aff553cbc69a245f78036c30e2b549ca1f53d543
Sandipan54 <130487031+Sandipan54@users.noreply.github.com>
2025-04-11 21:12:27 +0530

    lancer.py

--- lancer.py
+++ lancer.py
@@ -6,7 +6,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from keep_alive import keep_alive
 
-BOT_TOKEN = "7688718088:AAGg83EwtG_u_9vfrP0huqUBBpMuf2UWIn4"
+BOT_TOKEN = "8104559519:AAFAK8O8PddOuOC6xhewMbVK3q2y47zx2qw"
 
 async def lancer_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
